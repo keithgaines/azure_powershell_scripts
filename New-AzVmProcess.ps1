3740f28703a79b83f1e643f8cc8d6a17092351d4
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-02-10 15:59:04 -0600

    Update and rename New-AzVm to New-AzVmProcess.ps1

---
+++ New-AzVmProcess.ps1
@@ -0,0 +1,18 @@
+
+# creates new resource group with user provided paramaters stored in group name and location variables
+$resourceGroupName = Read-Host -Prompt 'Input the resource group name'
+$location = Read-Host -Prompt 'Input the resource group location'
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# creates new virtual machine in Azure with given parameters
+
+New-AzVm `
+    -ResourceGroupName "ps1ResourceGroup" `
+    -Name "ps1vm01" `
+    -Location "East US" `
+    -VirtualNetworkName "ps1Vnet" `
+    -SubnetName "ps1Subnet" `
+    -SecurityGroupName "ps1NetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389
